16ee9bd98922b4ed0e140c1fbcdb383c23d51bfd
AyaAdel <aya.adel.wd@gmail.com>
2020-11-01 15:35:55 +0200

    WIP Header

--- assets/src/scripts/script.js
+++ assets/src/scripts/script.js
@@ -1,6 +1,8 @@
 const menubar = document.getElementById('menubar');
 const close = document.getElementById('close');
 const toggleMenu = document.getElementById('toggleMenu');
+const eng = document.getElementById('eng');
+const dt = document.getElementById('dt');
 
 const displayMenu = () => {
   menubar.classList.add('hide');
@@ -12,4 +14,14 @@ const hideMenu = () => {
   close.classList.add('hide');
   toggleMenu.classList.add('hide');
   menubar.classList.remove('hide');
+};
+
+const switchEng = () => {
+  eng.classList.add('active');
+  dt.classList.remove('active');
+};
+
+const switchDt = () => {
+  eng.classList.remove('active');
+  dt.classList.add('active');
 };
